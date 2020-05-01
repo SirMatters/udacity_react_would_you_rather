f86e254590f87a6678553d9e43553faac7a75cba
SirMatters <perov.krll@gmail.com>
2020-05-01 11:17:28 +0300

    props added

--- book_reads/contacts/src/App.js
+++ book_reads/contacts/src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>Hello, world</p>
-      </header>
-    </div>
-  );
+const people1 = [{ name: 'Karen1' }, { name: 'Jack1' }, { name: 'Marco1' }];
+const people2 = [{ name: 'Karen2' }, { name: 'Jack2' }, { name: 'Marco2' }];
+const people3 = [{ name: 'Karen3' }, { name: 'Jack3' }, { name: 'Marco3' }];
+
+class ContactList extends Component {
+  render() {
+    const people = this.props.contacts;
+    return (
+      <div className='contactList'>
+        <ol>
+          {people.map((p) => (
+            <li key={p.name}>{p.name}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <ContactList contacts={people1} />
+        <ContactList contacts={people2} />
+        <ContactList contacts={people3} />
+      </div>
+    );
+  }
 }
 
 export default App;
