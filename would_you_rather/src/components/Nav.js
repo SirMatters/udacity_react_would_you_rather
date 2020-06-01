81786def46e6659da2339554e8a9e5f5e7128c37
SirMatters <perov.krll@gmail.com>
2020-06-01 22:23:05 +0300

    add nav routing

--- would_you_rather/src/components/Nav.js
+++ would_you_rather/src/components/Nav.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 class Nav extends React.Component {
   render() {
     const { users, authedUser } = this.props;
     return (
       <nav>
-        <div className='nav-left'>
-          <button className='nav-link'>Questions</button>
-          <button className='nav-link'>Leaderboard</button>
-        </div>
+        <ul className='nav-left'>
+          <li>
+            <NavLink to='/' activeClassName='active' className='nav-link'>
+              Questions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/leaderboard'
+              activeClassName='active'
+              className='nav-link'
+            >
+              Leaderboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/new' activeClassName='active' className='nav-link'>
+              New
+            </NavLink>
+          </li>
+        </ul>
         <div className='nav-right'>
           <img
             className='user-avatar-nav'
